1cdd0e07b8ca2e33efb76cb4134632bc8c0a74e0
Alok Kumar <alok.kumar@ovyo.com>
2025-04-17 19:39:58 +0530

    Initial Commit

---
+++ DemoRegistration/DemoRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRegistrationApp.swift
+//  DemoRegistration
+//
+//  Created by Alok Kumar on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
