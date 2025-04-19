5725be600869833f3258c073637c83a3a1b2bfcc
Alok Kumar <alok.kumar@ovyo.com>
2025-04-19 15:52:33 +0530

    dashboard view updation

--- DemoRegistration/Presentation/Views/DashboardView.swift
+++ DemoRegistration/Presentation/Views/DashboardView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct DashboardView: View {
+    
+    @State var stepper: Double = 1
+    
     var body: some View {
         VStack {
-            Spacer()
-            
-            Text("Content Area")
-                .font(.title)
-                .padding(.top)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("January")
+                    .font(.system(size: 18, weight: .bold))
+                    .padding(15)
+                
+                HStack {
+                    Text("$ 500")
+                        .font(.system(size: 36, weight: .heavy))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chart.bar.xaxis.ascending")
+                        .font(.system(size: 24, weight: .medium))
+                }
+                .padding(.horizontal, 15)
+                
+                HStack {
+                    ProgressView(value: 0.6)
+                        .progressViewStyle(LinearProgressViewStyle())
+                        .tint(.white)
+                        .background(.white.opacity(0.3))
+                        .clipShape(.capsule)
+                    
+                    Spacer()
+                    
+                    Text("60%")
+                }
+                .padding(.horizontal, 15)
+                .padding(.top, 15)
+                
+                Text("Daily Spend Target: $16.76")
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 15)
+            }
+            .background(Color.blue)
+            .clipShape(.rect(cornerRadius: 15))
+            .padding(15)
+            .shadow(color: Color.black.opacity(0.5), radius: 5, x: 2, y: 4)
+            .foregroundStyle(Color.white)
             
             Spacer()
             
